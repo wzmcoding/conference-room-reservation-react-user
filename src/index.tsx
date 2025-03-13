36ea85863729f98f75185666968f8f4e713616b8
wzm <2791063637@qq.com>
2025-03-13 16:21:40 +0800

    feat: 添加会议室列表功能

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { UpdatePassword } from './pages/update_password/UpdatePassword';
 import { ErrorPage } from './pages/errror/ErrorPage';
 import { Index } from './pages/index';
 import { UpdateInfo } from './pages/update_info/UpdateInfo';
+import {Menu} from "./pages/menu/Menu";
+import {MeetingRoomList} from "./pages/meeting_room_list/MeetingRoomList";
+import {BookingHistory} from "./pages/booking_history/BookingHistory";
 
 const routes = [
   {
@@ -18,6 +21,24 @@ const routes = [
         path: 'update_info',
         element: <UpdateInfo />,
       },
+      {
+        path: '/',
+        element: <Menu/>,
+        children: [
+          {
+            path: '/',
+            element: <MeetingRoomList/>
+          },
+          {
+            path: 'meeting_room_list',
+            element: <MeetingRoomList/>
+          },
+          {
+            path: 'booking_history',
+            element: <BookingHistory/>
+          }
+        ]
+      }
     ]
   },
   {
@@ -33,7 +54,7 @@ const routes = [
     element: <UpdatePassword />,
   }
 ];
-const router = createBrowserRouter(routes);
+export const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
